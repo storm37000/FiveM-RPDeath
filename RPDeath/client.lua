351126b80a34b2bad66f971e7cd6cfb8a351620e
storm37000 <6331913+storm37000@users.noreply.github.com>
2021-08-03 17:18:27 -0400

    bring this resource up to modern times

--- RPDeath/client.lua
+++ RPDeath/client.lua
@@ -2,15 +2,20 @@ RegisterNetEvent('RPD:allowRespawn')
 RegisterNetEvent('RPD:allowRevive') 
 RegisterNetEvent('RPD:toggleDeath')
 
-local reviveWaitPeriod = 300 -- How many seconds to wait before allowing player to revive themselves
+--local reviveWaitPeriod = 1 -- How many seconds to wait before allowing player to revive themselves
 local RPDeathEnabled = true  -- Is RPDeath enabled by default? (/toggleDeath changes this value.)
 
+local function ShowNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
 
 
 -- Turn off automatic respawn here instead of updating FiveM file.
 AddEventHandler('onClientMapStart', function()
 	Citizen.Trace("RPDeath: Disabling autospawn...")
-	exports.spawnmanager:spawnPlayer() -- Ensure player spawns into server.
+	--exports.spawnmanager:spawnPlayer() -- Ensure player spawns into server.
 	Citizen.Wait(2500)
 	exports.spawnmanager:setAutoSpawn(false)
 	Citizen.Trace("RPDeath: Autospawn disabled!")
@@ -19,11 +24,12 @@ end)
 
 local allowRespawn = false
 local allowRevive = false
-local diedTime = nil
+--local diedTime = nil
+local notif = false
 
 
 AddEventHandler('RPD:allowRespawn', function(from)
-	TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "Respawned")
+	ShowNotification("Respawned")
 	allowRespawn = true
 end)
 
@@ -35,24 +41,24 @@ AddEventHandler('RPD:allowRevive', function(from)
 	end
 
 	-- Trying to revive themselves?
-	if(GetPlayerServerId(PlayerId()) == from and diedTime ~= nil)then
-		local waitPeriod = diedTime + (reviveWaitPeriod * 1000)
-		if(GetGameTimer() < waitPeriod)then
-			local seconds = math.ceil((waitPeriod - GetGameTimer()) / 1000)
-			local message = ""
-			if(seconds > 60)then
-				local minutes = math.floor((seconds / 60))
-				seconds = math.ceil(seconds-(minutes*60))
-				message = minutes.." minutes "
-			end
-			message = message..seconds.." seconds"
-			TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "You must wait before reviving yourself, you have ^5"..message.."^0 remaining.")
-			return		
-		end
-	end
+--	if(GetPlayerServerId(PlayerId()) == from and diedTime ~= nil)then
+--		local waitPeriod = diedTime + (reviveWaitPeriod * 1000)
+--		if(GetGameTimer() < waitPeriod)then
+--			local seconds = math.ceil((waitPeriod - GetGameTimer()) / 1000)
+--			local message = ""
+--			if(seconds > 60)then
+--				local minutes = math.floor((seconds / 60))
+--				seconds = math.ceil(seconds-(minutes*60))
+--				message = minutes.." minutes "
+--			end
+--			message = message..seconds.." seconds"
+--			TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "You must wait before reviving yourself, you have ^5"..message.."^0 remaining.")
+--			return		
+--		end
+--	end
 
 	-- Revive the player.
-	TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "Revived")
+	ShowNotification("Revived")
 	allowRevive = true
 end)
 
@@ -62,12 +68,14 @@ AddEventHandler('RPD:toggleDeath', function(from)
 		TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "RPDeath enabled.")
 	else
 		TriggerEvent('chatMessage', "RPDeath", {200,0,0}, "RPDeath disabled.")
+		allowRespawn = false
+		allowRevive = false
+		notif = false
 	end
 end)
 
 
-
-function revivePed(ped)
+local function revivePed(ped)
 	local playerPos = GetEntityCoords(ped, true)
 
 	NetworkResurrectLocalPlayer(playerPos, true, true, false)
@@ -75,101 +83,61 @@ function revivePed(ped)
 	ClearPedBloodDamage(ped)
 end
 
+--local respawnCount = 0
 
-function respawnPed(ped,coords)
-	SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
-	NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, false) 
-
-	SetPlayerInvincible(ped, false) 
-
-	TriggerEvent('playerSpawned', coords.x, coords.y, coords.z, coords.heading)
-	ClearPedBloodDamage(ped)
-end
-
+--createSpawnPoint(-448, -448, -340, -329, 35.5, 0) -- Mount Zonah
+--createSpawnPoint(372, 375, -596, -594, 30.0, 0)   -- Pillbox Hill
+--createSpawnPoint(335, 340, -1400, -1390, 34.0, 0) -- Central Los Santos
+--createSpawnPoint(1850, 1854, 3700, 3704, 35.0, 0) -- Sandy Shores
+--createSpawnPoint(-247, -245, 6328, 6332, 33.5, 0) -- Paleto
+--createSpawnPoint(1152, 1156, -1525, -1521, 34.9, 0) -- St. Fiacre
 
 Citizen.CreateThread(function()
-	local respawnCount = 0
-	local spawnPoints = {}
 	local playerIndex = NetworkGetPlayerIndex(-1) or 0
 
-
-	math.randomseed(playerIndex)
-
-	function createSpawnPoint(x1,x2,y1,y2,z,heading)
-		local xValue = math.random(x1,x2) + 0.0001
-		local yValue = math.random(y1,y2) + 0.0001
-
-		local newObject = {
-			x = xValue,
-			y = yValue,
-			z = z + 0.0001,
-			heading = heading + 0.0001
-		}
-		table.insert(spawnPoints,newObject)
-	end
-
-	createSpawnPoint(-448, -448, -340, -329, 35.5, 0) -- Mount Zonah
-	createSpawnPoint(372, 375, -596, -594, 30.0, 0)   -- Pillbox Hill
-	createSpawnPoint(335, 340, -1400, -1390, 34.0, 0) -- Central Los Santos
-	createSpawnPoint(1850, 1854, 3700, 3704, 35.0, 0) -- Sandy Shores
-	createSpawnPoint(-247, -245, 6328, 6332, 33.5, 0) -- Paleto
-	--createSpawnPoint(1152, 1156, -1525, -1521, 34.9, 0) -- St. Fiacre
-
 	while true do
-		Wait(0)
+		Wait(500)
 		local ped = GetPlayerPed(-1)
 		
 		if (RPDeathEnabled) then
 
 			if (IsEntityDead(ped)) then
-				if(diedTime == nil)then
-					diedTime = GetGameTimer()
-				end
-
+				--if(diedTime == nil)then
+				--	diedTime = GetGameTimer()
+				--end
 
 				SetPlayerInvincible(ped, true)
 				SetEntityHealth(ped, 1)
+
+				if not notif then
+					notif = true
+					Citizen.CreateThread(function()
+						while IsEntityDead(GetPlayerPed(-1)) do
+							ShowNotification("You are dead, say /revive to respawn at your death location, or say /respawn to respawn elsewhere.")
+							Wait(5000)
+						end
+					end)
+				end
 				
-				if (allowRespawn) then 
-					local coords = spawnPoints[math.random(1,#spawnPoints)]
-
-					respawnPed(ped, coords)
-
+				if (allowRespawn) then
+					notif = false
+					exports.spawnmanager:spawnPlayer()
 			  		allowRespawn = false
-			  		diedTime = nil
-					respawnCount = respawnCount + 1
-					math.randomseed( playerIndex * respawnCount )
+			  		--diedTime = nil
+					--respawnCount = respawnCount + 1
 
 				elseif (allowRevive) then
+					notif = false
 					revivePed(ped)
-
 					allowRevive = false	
-		  			diedTime = nil
-					Wait(0)
-				else
-		  			Wait(0)
+		  			--diedTime = nil
 				end
-			else
-		  		allowRespawn = false
-		  		allowRevive = false	
-		  		diedTime = nil		
-				Wait(0)
-			end
-
-
-		else 
-			if IsEntityDead(ped) then
-				Wait(3000) 
-
-				local coords = spawnPoints[math.random(1,#spawnPoints)]
-
-				respawnPed(ped,coords)
-
-				respawnCount = respawnCount + 1
-				math.randomseed( playerIndex * respawnCount )
-				
+--			else
+--		  		allowRespawn = false
+--		  		allowRevive = false	
+		  		--diedTime = nil		
+--				Wait(0)
 			end
 		end
-
 	end
 end)
--- RPDeath/server.lua
+++ RPDeath/server.lua
@@ -1,51 +1,22 @@
-AddEventHandler('chatMessage', function(from,name,message)
-	if(message:sub(1,1) == "/") then
+RegisterCommand("respawn", function(source, args)
+	TriggerClientEvent('RPD:allowRespawn', source)
+end)
+RegisterCommand("toggleDeath", function(source, args)
+	TriggerClientEvent('RPD:toggleDeath', source)
+end)
+RegisterCommand("revive", function(from, args)
+	if (args[1] ~= nil and IsPlayerAceAllowed(from, "reviveothers")) then
+		local playerID = tonumber(args[1])
 
-		local args = stringsplit(message, " ")
-		local cmd = args[1]
-
-
-		if (cmd == "/respawn") then
-			CancelEvent()
-			TriggerClientEvent('RPD:allowRespawn', from)
+		if(playerID == nil or playerID == 0 or GetPlayerName(playerID) == nil) then
+			TriggerClientEvent('chatMessage', from, "RPDeath", {200,0,0} , "Invalid PlayerID")
+			return
 		end
 
-		if (cmd == "/toggleDeath") then
-			CancelEvent()
-			TriggerClientEvent('RPD:toggleDeath', from)
-		end
+		TriggerClientEvent('RPD:allowRevive', playerID, from)
 
-		if (cmd == "/revive") then
-			CancelEvent()
-
-			if (args[2] ~= nil) then
-				local playerID = tonumber(args[2])
-
-				if(playerID == nil or playerID == 0 or GetPlayerName(playerID) == nil) then
-					TriggerClientEvent('chatMessage', from, "RPDeath", {200,0,0} , "Invalid PlayerID")
-					return
-				end
-
-				TriggerClientEvent('RPD:allowRevive', playerID, from)
-
-				TriggerClientEvent('chatMessage', from, "RPDeath", {200,0,0} , "Player revived")
-			else
-				TriggerClientEvent('RPD:allowRevive', from, from)
-			end
-		end
+		TriggerClientEvent('chatMessage', from, "RPDeath", {200,0,0} , "Player revived")
+	else
+		TriggerClientEvent('RPD:allowRevive', from, from)
 	end
 end)
-
-
--- String splits by the separator.
-function stringsplit(inputstr, sep)
-    if sep == nil then
-            sep = "%s"
-    end
-    local t={} ; i=1
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-            t[i] = str
-            i = i + 1
-    end
-    return t
-end
